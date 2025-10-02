56aaa96aebdea9b11c41e5c24d4a7dccb6d77cbd
Yarick126 <gadhikurbanovy@mail.ru>
2025-10-02 15:05:34 +0300

    nvm

--- src/components/ui/Button/Button.js
+++ src/components/ui/Button/Button.js
@@ -1,12 +1,13 @@
 import './button-style.scss'
 
-const Button = ({text , className, onClick , title = ''}) => {
+const Button = ({text , className, onClick , title = '', type = 'submit'}) => {
 
     const ButtonComponent = document.createElement('button')
     ButtonComponent.textContent = text ?? text
     ButtonComponent.className = className ?? className
     ButtonComponent.addEventListener('click', onClick)
     ButtonComponent.setAttribute('title' , title)
+    ButtonComponent.setAttribute('type' , type)
     return ButtonComponent
 }
 
